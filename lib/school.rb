58e2a380c7a264ca2f61c91fcc378dd9a6f19519
Donna Torres <todonnatorres@icloud.com>
2020-06-10 01:29:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,24 +1,70 @@
 class School
-  attr_reader :roster
+  attr_accessor :roster
   
-  def initialize(name)
+  def initialize(phil) 
     @roster = {}
   end
-  
   def add_student(name, grade)
-    # unless @roster [grade]
-      @roster[grade] ||= []
+    @roster[grade] ||= []
     @roster[grade] << name
   end
   def grade(number)
-    roster[number]
+    @roster[number]
   end
-  
   def sort
-    new_hash = {}
-    roster.keys.sort.each do |number|
-      new_hash[number] = roster[number].sort
+    hash = {}
+    @roster.keys.sort.each do |number|
+      hash[number] = @roster[number].sort
     end
-    new_hash
+    hash
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class School
+#   attr_reader :roster
+  
+#   def initialize(name)
+#     @roster = {}
+#   end
+  
+#   def add_student(name, grade)
+#     # unless @roster [grade]
+#       @roster[grade] ||= []
+#     @roster[grade] << name
+#   end
+#   def grade(number)
+#     roster[number]
+#   end
+  
+#   def sort
+#     new_hash = {}
+#     roster.keys.sort.each do |number|
+#       new_hash[number] = roster[number].sort
+#     end
+#     new_hash
+#   end
+# end
